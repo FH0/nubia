6180a073b7543acec52e9f001e1fce4dbcba32d8
FH0 <32861476+FH0@users.noreply.github.com>
2017-11-26 00:04:29 +0800

    Update sussr.sh

--- sussr.sh
+++ sussr.sh
@@ -1,15 +1,9 @@
-apt update -y
-pkg install wget unzip -y
+pkg install unzip -y
 /data/data/com.termux/files/usr/bin/wget https://raw.githubusercontent.com/FH0/nubia/master/sussr.zip
-su
-
-cd
 cp /data/data/com.termux/files/home/sussr.zip /data/data
 cd /data/data
 unzip sussr.zip
-chmod 0777 sussr
 cd sussr/
-sh start.sh
-echo ssr='su
-
-sh /data/data/com.wg.ssr/files/sussr/start.sh' >> /data/data/com.termux/files/usr/etc/profile
+chmod 777 *
+sh /data/data/sussr/start.sh
+echo 'alias ssr="sh /data/data/com.wg.ssr/files/sussr/start.sh"' >> /data/data/com.termux/files/usr/etc/profile
