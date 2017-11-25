89911f5ea0b9f4cd5a311f08f374f0b36e072ec7
FH0 <32861476+FH0@users.noreply.github.com>
2017-11-25 18:01:38 +0800

    Add files via upload

---
+++ ssr.sh
@@ -0,0 +1,8 @@
+#set shadowsocksR
+yum install wget nano make tar screen unzip -y
+wget https://raw.githubusercontent.com/FH0/nubia/master/ssr.zip && unzip ssr.zip && cd SSR* && bash install.sh
+cd /usr/local/shadowsocksr
+python mujson_mgr.py -a -u 1 -p 80 -k 239 -m chacha20 -O auth_sha1_v4 -o http_simple -t 700
+python mujson_mgr.py -a -u 2 -p 8080 -k 239 -m chacha20 -O auth_sha1_v4 -o http_simple -t 70
+python mujson_mgr.py -a -u 3 -p 53 -k 239 -m chacha20 -O auth_sha1_v4 -o http_simple -t 70
+bash /usr/local/shadowsocksr/logrun.sh
