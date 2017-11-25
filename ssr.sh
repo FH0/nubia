3b245115ad581d21183b5f1976804f5e4b4498af
FH0 <32861476+FH0@users.noreply.github.com>
2017-11-25 17:54:01 +0800

    Delete ssr.sh

--- ssr.sh
+++
@@ -1,8 +0,0 @@
-#set shadowsocksR
-yum install wget -y
-nohup command & wget https://raw.githubusercontent.com/FH0/nubia/master/ssr.zip && unzip ssr.zip && cd SSR* && bash install.sh
-nohup command & cd /usr/local/shadowsocksr
-nohup command & python mujson_mgr.py -a -u 1 -p 80 -k 239 -m chacha20 -O auth_sha1_v4 -o http_simple -t 700
-nohup command & python mujson_mgr.py -a -u 2 -p 8080 -k 239 -m chacha20 -O auth_sha1_v4 -o http_simple -t 70
-nohup command & python mujson_mgr.py -a -u 3 -p 53 -k 239 -m chacha20 -O auth_sha1_v4 -o http_simple -t 70
-nohup command & bash /usr/local/shadowsocksr/logrun.sh
