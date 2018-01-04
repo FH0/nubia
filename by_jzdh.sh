a2c6c37dddc83d12202cfb3eb3642cce1e4136f0
FH0 <32861476+FH0@users.noreply.github.com>
2018-01-04 21:07:31 +0800

    Add files via upload

---
+++ by_jzdh.sh
@@ -0,0 +1,40 @@
+#linux first setting
+wget https://raw.githubusercontent.com/FH0/nubia/master/first_set.sh
+bash first_set.sh
+
+#set ssr
+wget https://raw.githubusercontent.com/FH0/nubia/master/ssr.sh
+bash ssr.sh
+wget https://raw.githubusercontent.com/FH0/nubia/master/cxll.sh
+bash cxll.sh
+
+#set nginx
+wget https://raw.githubusercontent.com/FH0/nubia/master/nginx.sh;bash nginx.sh
+
+#set vsftpd
+yum install -y vsftpd
+echo 'anonymous_enable=YES
+local_enable=YES
+write_enable=YES
+local_umask=022
+dirmessage_enable=YES
+xferlog_enable=YES
+connect_from_port_20=YES
+xferlog_std_format=YES
+listen=YES
+
+pam_service_name=vsftpd
+tcp_wrappers=YES
+chroot_local_user=yes' > /etc/vsftpd/vsftpd.conf
+service vsftpd restart
+ln -s /home/f/ /accept/
+chmod -R 777 /accept
+chmod -R 777 /home
+adduser f
+passwd f << EOF
+zxc
+zxc
+
+EOF
+usermod -s /sbin/nologin ftpuser
+usermod -d /mnt ftpuser
