37e9e3266dfd762dcc16004fcd9b64f92232199c
FH0 <32861476+FH0@users.noreply.github.com>
2018-03-07 04:41:38 +0000

    Delete sussr.sh

--- sussr.sh
+++
@@ -1,9 +0,0 @@
-pkg install unzip -y
-/data/data/com.termux/files/usr/bin/wget https://raw.githubusercontent.com/FH0/nubia/master/sussr.zip
-cp /data/data/com.termux/files/home/sussr.zip /data/data
-cd /data/data
-unzip sussr.zip
-cd sussr/
-chmod 777 *
-sh /data/data/sussr/start.sh
-echo 'alias ssr="sh /data/data/com.wg.ssr/files/sussr/start.sh"' >> /data/data/com.termux/files/usr/etc/profile
