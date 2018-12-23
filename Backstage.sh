2b077a5082b3778523c17efc8b49653793a68fe9
FH0 <clbcjt@gmail.com>
2018-12-23 13:04:51 +0800

    update

--- Backstage.sh
+++ Backstage.sh
@@ -49,7 +49,7 @@ install_zip(){
 }
 
 install_bbr() {
-    if (($(uname -r | grep "^4"))) && (($(uname -r | awk -F "." '{print $2}')>=9));then
+    if uname -r | grep -q "^4" && (($(uname -r | awk -F "." '{print $2}')>=9));then
         sed -i '/^net.core.default_qdisc=fq$/d' /etc/sysctl.conf
         sed -i '/^net.ipv4.tcp_congestion_control=bbr$/d' /etc/sysctl.conf
         echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
